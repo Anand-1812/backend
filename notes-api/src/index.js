11f185ba68d32b450f5dbff8903a8eb8e41ec918
Anand-1812 <anand.work2005@gmail.com>
2026-01-20 22:45:34 +0530

    add cors in inde ex

--- notes-api/src/index.js
+++ notes-api/src/index.js
@@ -15,6 +15,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors({
+  origin: "http:localhost:5173",
+  credentials: true,
+}))
+
 app.get("/api", (_req, res) => {
   res.send("Backend is running")
 });
