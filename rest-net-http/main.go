b3899b24e327582a61b907c3474996b703534004
Anand-1812 <anand.work2005@gmail.com>
2026-06-11 14:35:19 +0530

    curd with the db

--- rest-net-http/main.go
+++ rest-net-http/main.go
@@ -10,6 +10,37 @@ import (
 )
 
 var db *sql.DB
+type Album struct {
+	ID int64
+	Title string
+	Artist string
+	Price float32
+}
+
+func albumByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumByArtist %q: %v", err)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price);err != nil {
+			return nil, fmt.Errorf("albumByArtist %q: %v", err)
+		}
+
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumByArtist %q: %v", err)
+	}
+
+	return albums, nil
+}
 
 func main() {
 	cfg := mysql.NewConfig()
@@ -31,4 +62,11 @@ func main() {
 	}
 
 	fmt.Println("connected :)")
+
+	albums, err := albumByArtist("weekend")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Album found: %v\n", albums)
 }
